139fb6a3a2ae6fb1971e1229c1d6355f81110b18
nightkiler12 <49460403+nightkiler12@users.noreply.github.com>
2022-04-23 13:23:41 -0700

    First Working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import  ReactDOM  from "react-dom";
+import { createRoot }  from "react-dom/client";
+import { StrictMode } from "react";
+
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+const rootElement = document.getElementById('#root');
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+
+        <App />
+
+    </StrictMode>
+
+
+);
